63d66588503d598486c89ca73830b20818793ca4
yzy <1197988245@qq.com>
2018-02-06 16:17:01 +0800

    添加用户管理，学生管理

---
+++ src/app/admin/student-dialog/student-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-dialog',
+  templateUrl: './student-dialog.component.html',
+  styleUrls: ['./student-dialog.component.scss']
+})
+export class StudentDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
